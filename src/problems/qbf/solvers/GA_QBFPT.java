86a6c484f1353aec5eb9a23a033b600e2475272d
Vitor Satoru <vitorsatoru@hotmail.com>
2020-11-21 18:04:35 -0300

    Adicionado calculo das triplas proibidas

--- src/problems/qbf/solvers/GA_QBFPT.java
+++ src/problems/qbf/solvers/GA_QBFPT.java
@@ -4,7 +4,39 @@ import solutions.Solution;
 
 import java.io.IOException;
 
+import java.util.Arrays;
+
 public class GA_QBFPT extends GA_QBF {
+
+    private final Integer triples[][];
+
+    private final Integer pi1_g = 131;
+    private final Integer pi2_g = 1031;
+    private final Integer pi1_h = 193;
+    private final Integer pi2_h = 1093;
+
+    private Integer l(Integer u, Integer pi1, Integer pi2) {
+        return 1 + ((pi1 * (u - 1) + pi2) % ObjFunction.getDomainSize());
+    }
+
+    private Integer g(Integer u, Integer l_u) {
+        if (l_u != u) {
+            return l_u;
+        } else {
+            return 1 + (l_u % ObjFunction.getDomainSize());
+        }
+    }
+
+    private Integer h(Integer u, Integer l_u, Integer g_u) {
+        int size = ObjFunction.getDomainSize();
+        if (l_u != u && l_u != g_u) {
+            return l_u;
+        } else if ((1 + (l_u % size)) != u && (1 + (l_u % size)) != g_u) {
+            return 1 + (l_u % size);
+        } else {
+            return 1 + ((l_u + 1) % size);
+        }
+    }
     /**
      * Constructor for the GA_QBF class. The QBF objective function is passed as
      * argument for the superclass constructor.
@@ -18,12 +50,28 @@ public class GA_QBFPT extends GA_QBF {
      */
     public GA_QBFPT(Integer generations, Integer popSize, Double mutationRate, String filename) throws IOException {
         super(generations, popSize, mutationRate, filename);
+
+        Integer l_u_g, l_u_h, g_u, h_u;
+        int size = ObjFunction.getDomainSize();
+        triples = new Integer[size][3];
+
+        for (int u = 1; u <= size; u++) {
+            l_u_g = l(u,pi1_g,pi2_g);
+            l_u_h = l(u,pi1_h,pi2_h);
+            g_u = g(u,l_u_g);
+            h_u = h(u,l_u_h,g_u);
+            triples[u-1][0] = u - 1;
+            triples[u-1][1] = g_u - 1;
+            triples[u-1][2] = h_u - 1;
+            Arrays.sort(triples[u-1]);
+        }
+
     }
 
     public static void main(String[] args) throws IOException {
 
         long startTime = System.currentTimeMillis();
-        GA_QBFPT ga = new GA_QBFPT(1000, 100, 1.0 / 100.0, "instances/qbf100");
+        GA_QBFPT ga = new GA_QBFPT(1000, 100, 1.0 / 100.0, "instances/qbf020");
         Solution<Integer> bestSol = ga.solve();
         System.out.println("maxVal = " + bestSol);
         long endTime = System.currentTimeMillis();
